3439bb3e6a5ab53d1b316b0c19a4d54f0de59ab3
Vlados178spb <a89043335555@gmail.com>
2026-04-17 01:45:41 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import asyncio
 import logging
 from bot import dp, bot  # Твой основной файл бота
-from services.services.processor import AladdinProcessor
+from services.processor import AladdinProcessor
 
 # Логирование для отслеживания работы в Railway или Termux
 logging.basicConfig(level=logging.INFO)
