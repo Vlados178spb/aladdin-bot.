4d42a8f7c841e0b017ab825794516ac2a41bf9b7
Vlados178spb <a89043335555@gmail.com>
2026-04-18 02:51:12 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,9 @@
-BOT_TOKEN = "8694698903:AAHK51pTIQo4TFcBBF1RbL4Kh5OZRiLGTiM"
+import os
+from dotenv import load_dotenv
 
-ODDS_API_KEY = "2be3c040e725dabfe695ae282049a8b0"
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+ODDS_API_KEY = os.getenv("ODDS_API_KEY")
+ODDS_API_IO_KEY = os.getenv("ODDS_API_IO_KEY")
